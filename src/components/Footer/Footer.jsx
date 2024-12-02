b13b1072633246c244c4ae11fdbd5c17ad2406df
Roger Gaylord IV <rgaylordiv@gmail.com>
2024-12-01 23:43:05 -0800

    component fonts, icon, footer, media query

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,12 @@ import './Footer.css';
 
 export default function Footer(){
     return(
-        <>
-        </>
+        <footer className="footer">
+            <div className="footer__contents">
+                <div className="footer__author">
+                    <p className="footer__text">Developed By Roger Gaylord</p>
+                </div>
+            </div>
+        </footer>
     )
 }

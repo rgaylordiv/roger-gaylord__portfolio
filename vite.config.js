169e8dbd4064ee71ad3fe274dff93fa8316dac17
Roger Gaylord IV <rgaylordiv@gmail.com>
2024-12-01 17:23:53 -0800

    media query fixes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/roger-gaylord__portfolio/",
+  server: {
+    port: 3001,
+  },
+  build: {
+    outDir: "dist",
+  },
 })
